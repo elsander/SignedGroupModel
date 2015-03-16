8127db349c9f052d458dbdaa35540a85e7cd8397
Elizabeth Sander <esander@friar.(none)>
2015-03-16 17:49:54 -0500

    make example run shorter

--- CCode/example.sh
+++ CCode/example.sh
@@ -3,6 +3,6 @@
 # 110 = number of species
 # ../Data/TatooshAll.txt = path to adjacency matrix
 # 587184 = random seed
-# 50000 = number of MCMC steps (should probably be higher for a network this size)
+# 5000 = number of MCMC steps (should probably be higher for a network this size, maybe 100000 and with multiple runs)
 # 20 = number of MCMC chains
-./FindGroups 110 ../Data/TatooshAll.txt 587184 50000 20
+./FindGroups 110 ../Data/TatooshAll.txt 587184 5000 20
